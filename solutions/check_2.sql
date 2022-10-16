ac3b21d40f0299402ec06b0db9a063a71682e609
Itay.Talias <itay.talias@gmail.com>
2022-10-16 19:05:38 +0300

    checks

--- solutions/check_2.sql
+++ solutions/check_2.sql
@@ -1,1 +1,5 @@
 USE sql_intro;
+
+SELECT item_purchased, amount, name, industry
+FROM transaction AS tr, company AS cu
+WHERE tr.company_id = cu.id;
--- solutions/check_4.sql
+++ solutions/check_4.sql
@@ -1,1 +1,16 @@
 USE sql_intro;
+
+
+-- SELECT s.s_name
+-- FROM student AS s,teacher AS t, student_teacher AS st
+-- WHERE s.s_id=st.student_id AND t.t_id=st.teacher_id AND t.t_name="Foster"
+ 
+SELECT *
+FROM student
+JOIN student_teacher
+ON s_id=student_teacher.student_id
+JOIN teacher
+ON t_id=student_teacher.teacher_id
+WHERE t_name="Foster"
+
+ 
